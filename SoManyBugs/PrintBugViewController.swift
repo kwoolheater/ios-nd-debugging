61151d1b360e24cc02fa3cf1789224cd1a169df8
Kiyoshi Woolheater <kiyoshiwoolheater@gmail.com>
2017-05-17 16:47:40 -0400

    find bug using print statements

--- SoManyBugs/PrintBugViewController.swift
+++ SoManyBugs/PrintBugViewController.swift
@@ -31,10 +31,13 @@ class PrintBugViewController: UIViewController {
     // MARK: Bug Functions
     
     func addBugToView() {
+        print("bug added")
         if bugs.count < maxBugs {
             let newBug = bugFactory.createBug()
             bugs.append(newBug)
+            view.addSubview(newBug)
             moveBugsAnimation()
+            print("bug created")
         }
     }
 
@@ -42,22 +45,22 @@ class PrintBugViewController: UIViewController {
         for bug in self.bugs {
             bug.removeFromSuperview()
         }
-        self.bugs.removeAll(keepCapacity: true)
+        self.bugs.removeAll(keepingCapacity: true)
     }
     
     // MARK: View Animations
     
     func moveBugsAnimation() {
-        UIView.animateWithDuration(moveDuration) {
+        UIView.animate(withDuration: moveDuration, animations: {
             for bug in self.bugs {
                 let randomPosition = CGPoint(x: CGFloat(arc4random_uniform(UInt32(UInt(self.view.bounds.maxX - bug.frame.size.width))) + UInt32(bug.frame.size.width/2)), y: CGFloat(arc4random_uniform(UInt32(UInt(self.view.bounds.maxY - bug.frame.size.height))) + UInt32(bug.frame.size.height/2)))
                 bug.frame = CGRect(x: randomPosition.x - bug.frame.size.width/1.5, y: randomPosition.y - bug.frame.size.height/1.5, width: BugFactory.bugSize.width, height: BugFactory.bugSize.height)
             }
-        }
+        }) 
     }
     
     func disperseBugsAnimation() {
-        UIView.animateWithDuration(disperseDuration, animations: { () -> Void in
+        UIView.animate(withDuration: disperseDuration, animations: { () -> Void in
             for bug in self.bugs {
                 let offScreenPosition = CGPoint(x: (bug.center.x - self.view.center.x) * 20, y: (bug.center.y - self.view.center.y) * 20)
                 bug.frame = CGRect(x: offScreenPosition.x, y: offScreenPosition.y, width: BugFactory.bugSize.width, height: BugFactory.bugSize.height)
@@ -70,17 +73,19 @@ class PrintBugViewController: UIViewController {
     // MARK: Actions
     
     @IBAction func popToMasterView() {
-        self.navigationController?.popToRootViewControllerAnimated(true)
+        self.navigationController?.popToRootViewController(animated: true)
     }
 }
 
 // MARK: - UIResponder
 
 extension PrintBugViewController {
-    override func canBecomeFirstResponder() -> Bool { return true }
-    override func motionEnded(motion: UIEventSubtype, withEvent event: UIEvent?) {
-        if motion == .MotionShake { disperseBugsAnimation() }
+    override var canBecomeFirstResponder : Bool { return true }
+    override func motionEnded(_ motion: UIEventSubtype, with event: UIEvent?) {
+        if motion == .motionShake { disperseBugsAnimation() }
     }
-    func handleSingleTap(recognizer: UITapGestureRecognizer) { addBugToView() }
+    func handleSingleTap(_ recognizer: UITapGestureRecognizer) {
+        print("tapped")
+        addBugToView() }
 }
 
